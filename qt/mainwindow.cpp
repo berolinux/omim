be2f33987bce041862181b1bf81d29f7f8ad4975
Ilya Zverev <zverik@textual.ru>
2017-02-07 15:15:39 +0300

    Fix code after rebasing to master

--- qt/mainwindow.cpp
+++ qt/mainwindow.cpp
@@ -697,7 +697,7 @@ void MainWindow::OnBuildStyle()
       QMainWindow::close();
     }
   }
-  catch (exception & e)
+  catch (std::exception & e)
   {
     QMessageBox msgBox;
     msgBox.setWindowTitle("Error");
@@ -723,7 +723,7 @@ void MainWindow::OnRecalculateGeomIndex()
       QMainWindow::close();
     }
   }
-  catch (exception & e)
+  catch (std::exception & e)
   {
     QMessageBox msgBox;
     msgBox.setWindowTitle("Error");
@@ -749,7 +749,7 @@ void MainWindow::OnGetStatistics()
     InfoDialog dlg(QString("Style statistics"), text, NULL);
     dlg.exec();
   }
-  catch (exception & e)
+  catch (std::exception & e)
   {
     QMessageBox msgBox;
     msgBox.setWindowTitle("Error");
@@ -768,7 +768,7 @@ void MainWindow::OnRunTests()
     InfoDialog dlg(QString("Style tests: ") + (res.first ? "OK" : "FAILED"), res.second, NULL);
     dlg.exec();
   }
-  catch (exception & e)
+  catch (std::exception & e)
   {
     QMessageBox msgBox;
     msgBox.setWindowTitle("Error");
